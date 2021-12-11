6f97dd9f45579fb3c912a86b74f5495d7cb466fb
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-10 19:18:01 -0500

    fixed category controller, added real text in place of dummy text

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -8,11 +8,11 @@ exports.homepage = async(req, res) => {
        const limitNumber = 5;
        const categories = await Category.find({}).limit(limitNumber);
        const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
-       const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
-       const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
-       const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
-       const food = { latest, thai, american, chinese };
-       res.render('index', { title: 'Recipe Blog - Home', categories, food }); 
+       const appetizers = await Recipe.find({ 'category': 'Appetizers' }).limit(limitNumber);
+       const breakfast = await Recipe.find({ 'category': 'Breakfast' }).limit(limitNumber);
+       const lunch = await Recipe.find({ 'category': 'Lunch' }).limit(limitNumber);
+       const food = { latest, appetizers, breakfast, lunch };
+       res.render('index', { title: 'Chef\'s Kiss - Home', categories, food }); 
     } catch (error) {
       res.status(500).send({ message: error.message || "Error Occurred" });  
     }
@@ -35,7 +35,7 @@ exports.exploreCategoriesById = async(req, res) => {
        let categoryId = req.params.id;
        const limitNumber = 20;
        const categoryById = await Recipe.find({ 'category': categoryId }).limit(limitNumber);
-       res.render('categories', { title: 'Recipe Blog - Categories', categoryById }); 
+       res.render('categories', { title: 'Chef\'s Kiss - Categories', categoryById }); 
     } catch (error) {
       res.status(500).send({ message: error.message || "Error Occurred" });  
     }
@@ -46,7 +46,7 @@ exports.showRecipe = async(req, res) => {
     try {
        let recipeId = req.params.id;
        const recipe = await Recipe.findById(recipeId);
-       res.render('recipe', { title: 'Recipe Blog - Recipe', recipe }); 
+       res.render('recipe', { title: 'Chef\'s Kiss - Recipe', recipe }); 
     } catch (error) {
       res.status(500).send({ message: error.message || "Error Occurred" });  
     }
@@ -58,7 +58,7 @@ exports.searchRecipe = async(req, res) => {
     try {
         let searchTerm = req.body.searchTerm;
         let recipe = await Recipe.find({ $text: { $search: searchTerm, $diacriticSensitive: true }});
-        res.render('search', { title: 'Recipe Blog - Search', recipe });
+        res.render('search', { title: 'Chef\'s Kiss - Search', recipe });
     } catch (error) {
         res.status(500).send({ message: error.message || "Error Occurred" });
     }
@@ -69,7 +69,7 @@ exports.exploreLatest = async(req, res) => {
     try {
        const limitNumber = 20;
        const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber)
-       res.render('explore-latest', { title: 'Recipe Blog - Explore Latest', recipe }); 
+       res.render('explore-latest', { title: 'Chef\'s Kiss - Explore Latest', recipe }); 
     } catch (error) {
       res.status(500).send({ message: error.message || "Error Occurred" });  
     }
@@ -81,7 +81,7 @@ exports.exploreRandom = async(req, res) => {
        let count = await Recipe.find().countDocuments();
        let random = Math.floor(Math.random() * count);
        let recipe = await Recipe.findOne().skip(random).exec();
-       res.render('explore-random', { title: 'Recipe Blog - Explore Random', recipe }); 
+       res.render('explore-random', { title: 'Chef\'s Kiss - Explore Random', recipe }); 
     } catch (error) {
       res.status(500).send({ message: error.message || "Error Occurred" });  
     }
@@ -90,7 +90,7 @@ exports.exploreRandom = async(req, res) => {
 exports.submitRecipe = async(req, res) => {
        const infoErrorsObj = req.flash('infoErrors');
        const infoSubmitObj = req.flash('infoSubmit');
-       res.render('submit-recipe', { title: 'Recipe Blog - Submit Recipe', infoErrorsObj, infoSubmitObj }); 
+       res.render('submit-recipe', { title: 'Chef\'s Kiss - Submit Recipe', infoErrorsObj, infoSubmitObj }); 
 }
 
 
